43c4b302e68424a5c6a1eb4086afbec92e55ff47
Bpazy <hanziyuan08@outlook.com>
2016-03-21 20:55:37 +0800

    dev

--- src/main/java/Utils/MethodHelpers.java
+++ src/main/java/Utils/MethodHelpers.java
@@ -1,10 +1,26 @@
 package Utils;
 
+import src.Subscribe;
 import src.SubscriberMethod;
 
-public class MethodHelpers {
-    public static SubscriberMethod[] findSubscriberMethods(Class<?> c) {
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
-        return null;
+public class MethodHelpers {
+    public static List<SubscriberMethod> findSubscriberMethods(Class<?> c) {
+        List<SubscriberMethod> list = new ArrayList<>();
+        Method[] declaredMethods = c.getDeclaredMethods();
+        for (Method m : declaredMethods) {
+            //遍历所有方法，取出含有注解Subscribe的方法，并将该类对象及方法存入methodMap中，供post(Object)方法invoke调用
+            Subscribe annotation = m.getDeclaredAnnotation(Subscribe.class);
+            if (annotation != null && annotation.chosen()) {
+                SubscriberMethod method = new SubscriberMethod();
+                method.setMethod(m);
+                method.setThreadMode(annotation.threadMode());
+                list.add(method);
+            }
+        }
+        return list;
     }
 }
